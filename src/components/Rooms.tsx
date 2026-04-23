8477930cd3ea37ce3c89dad00672ec9127c623ed
Harsh Veer <veerharsh919@gmail.com>
2026-04-24 00:13:23 +0530

    updated rooms

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -33,7 +33,7 @@ export default function Rooms() {
             id: 3,
             name: "Non-Ac Deluxe",
             price: "₹999",
-            image: "/images/WhatsApp Image 2026-04-23 at 23.40.51.jpg",
+            image: "/images/WhatsApp Image 2026-04-23 at 23.40.51.jpeg",
             description: "Experience luxury with a dedicated seating area and refined decor.",
             features: [
                 { icon: <BedDouble className="h-4 w-4" />, text: "1 Double Bed" },
